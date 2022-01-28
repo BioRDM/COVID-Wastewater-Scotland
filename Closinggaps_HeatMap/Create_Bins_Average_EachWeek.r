92720f7244f0dfcb1bfdc41f649a8d4bdc554273
Sumy <50071183+sumyvbaby@users.noreply.github.com>
2022-01-28 12:42:56 +0000

    Updated code

--- Closinggaps_HeatMap/Create_Bins_Average_EachWeek.r
+++ Closinggaps_HeatMap/Create_Bins_Average_EachWeek.r
@@ -1,6 +1,6 @@
 #Install the required packages and load the libraries
 install.packages("lubridate")
-install.packages("dplr")
+install.packages("dplyr")
 library(lubridate)
 library(dplr)
 
@@ -8,8 +8,8 @@ Data = read.table("/home/sbaby/Desktop/Latest_data/Data.csv", header =TRUE, sep 
 Data1 = as.data.frame(Data)
  # Extracting the Year month and week information from dates
 date =as.Date(Data1$Date, by="day")
-Data1$week=isoweek(date)
-month = format.Date(date, "%m")
+Data1$week = sprintf("%02d", isoweek(date)) # Format week as 2 digit for sorting
+Data1$month = sprintf("%02d", month(date)) # Format month as 2 digit for sorting
 Data1$month= format.Date(date, "%m")
 Data1$Year = year(date)
 
@@ -17,5 +17,4 @@ Data1$Year = year(date)
 
 aggregate(Data1$Million.Gene.Copies.Per.Person.per.Day, by=list(Site=Data1$Site, Year= Data1$Year,Month = Data1$month, Week = Data1$week), FUN=mean, na.rm=TRUE) ->Meanaggregate
 View(Meanaggregate)
-Meanaggregate1 <- as.matrix(Meanaggregate)
-write.table(Meanaggregate1,file="/home/sbaby/Desktop/Latest_data/Meanaggregate.csv")
+write.csv(Meanaggregate,file="/home/sbaby/Desktop/Latest_data/Meanaggregate.csv")
