949e50afa84e22377f5df9b1a66ac5e783545aea
Sumy <50071183+sumyvbaby@users.noreply.github.com>
2022-02-08 13:46:20 +0000

    Update the Code for new heatmap

--- prevalence-heatmap/draw-heatmap-recent.r
+++ prevalence-heatmap/draw-heatmap-recent.r
@@ -4,15 +4,22 @@ library("circlize")
 library("devtools")
 install_github("jokergoo/ComplexHeatmap")
 library("ComplexHeatmap")
-Data = read.table("/home/sbaby/Desktop/Latest_data/Sample.csv", header =TRUE, sep = ",", row.names = 1)  # Read the data file as a table. Sample.csv is the datafile after preprocessing
-Data1 = as.matrix(Data) #Converts the Data table in to matrix
-Data2 = Data1[,order(colnames(Data1))] # Sorts matrix in an ascending order according to the column labels
-write.table(Data2,file="/home/sbaby/Desktop/Latest_data/Columnsorted.csv")
-# Create a string matrix of dates ranging from 2020 to 2022
-Dates <- c("May-2020", "Jun-2020", "Jul-2020", "Aug-2020", "Sep-2020", "Oct-2020", "Nov-2020", "Dec-2020", "Jan-2021", "Feb-2021","Mar-2021", "Apr-2021","May-2021", "Jun-2021", "Jul-2021", "Aug-2021", "Sep-2021", "Oct-2021", "Nov-2021", "Dec-2021", "Jan-2022")
-col_fun = colorRamp2(c(0,658,1316,11386), c("yellow", "purple","orange", "blue")) # Set breakpoints and assigns color to those 
-# Set annotation for column at the specified column marks
-ha = HeatmapAnnotation(foo=anno_mark(at = c(1,3,21,34,55,79,101,121,140,161,186,212,235,257,280,301,323,345,366,389,405), labels = Dates,  labels_gp = gpar(fontsize=9),which = "column", side = "bottom"))
-pdf("Covid_Virus-2.pdf", width = 16, height = 15) #Save the Heatmap to pdf file
-Heatmap(Data2, name = "virus levels", col = col_fun,width = unit(30, "cm"),row_names_gp = gpar(fontsize = 7) , column_names_rot = 90, height = unit(33, "cm"),column_title = "Covid Virus concentrations at different sites along the timeline",column_title_gp = gpar(fontsize = 13,fontface = "bold"),na_col = "white", cluster_rows = FALSE, cluster_columns = FALSE , bottom_annotation = ha, show_column_names = FALSE,border = TRUE,border_gp = gpar(col = "grey"), row_names_side = "left", heatmap_legend_param = list( at = c(0,658,1316,11386), labels = c("Negative", "Weak Positive","Positive (DNQ)", "Positive")))
+library(tidyverse)
+
+Data = read.table("/home/sbaby/Desktop/Latest_data/Gaps_Closing1/Sample.csv", header =TRUE, sep = ",", row.names = 1) # Read the Preprocessed file (Normalized data arranged as mean value for each week)
+Data1 = as.matrix(Data)
+Data2 = Data1[,order(colnames(Data1))] # Sort columns 
+#Filtering by row name and columns
+Data2[row.names(Data2) %in% c("Allanfearn","Helensburgh","Carbarns","Hamilton","Philipshill","Seafield","East Calder","Linlithgow","Shieldhall","Dalmuir","Paisley","Daldowie","Nigg","Peterhead","Kirkwall","Lerwick","Hatton","Forfar","Stirling","Falkirk","Alloa","Meadowhead","Stevenston","Linlithgow","Dunfermline","Levenmouth","Kirkcaldy","Troqueer","Lockerbie","Galashiels","Hawick","Stornoway"),32:95] -> DF_new
+# Convert the row.names to a column to facilitate the arrange function for custom sort 
+rownames_to_column(DF_new, var = "rowname") -> DF
+# Custom sort
+DF %>% arrange(factor(rowname, levels = c("Allanfearn","Helensburgh","Carbarns","Hamilton","Philipshill","Seafield","East Calder","Linlithgow","Shieldhall","Dalmuir","Paisley","Daldowie","Nigg","Peterhead","Kirkwall","Lerwick","Hatton","Forfar","Stirling","Falkirk","Alloa","Meadowhead","Stevenston","Dunfermline","Levenmouth","Kirkcaldy","Troqueer","Lockerbie","Galashiels","Hawick","Stornoway"))) -> DF1
+# Convert it back to the row.names
+column_to_rownames(DF1, var = "rowname") -> DF2
+as.matrix(DF2) -> DF2
+#Specify the annotation file for the bottom
+ha = HeatmapAnnotation(foo=anno_mark(at = c(1,6,10,14,19,24,29,34,39,44,49,54,59,64), labels = Dates,  labels_gp = gpar(fontsize=3, fontface = "bold"),which = "column", side = "bottom"))
+jpeg(filename= "Covid_Heatmap.jpeg", width=1000, height = 700, units= "px",res= 300) # Save as jpeg with width 1000 pixels, height = 700 pixels, and res = 300 dpi
+Heatmap(DF2, col = col_fun1,row_names_gp = gpar(fontsize = 3, fontfamily = "Helvetica",fontface= "bold") , column_names_rot = 90,column_title = "Covid virus prevelance at different sites along the timeline",column_title_gp = gpar(fontsize = 5,fontface = "bold"),na_col = "white", cluster_rows = FALSE, cluster_columns = FALSE , bottom_annotation = ha, show_column_names = FALSE,border = TRUE,border_gp = gpar(col = "grey"), row_names_side = "left", heatmap_legend_param = list(title="Virus levels",title_gp = gpar(fontsize =3, fontfamily = "Helvetica", fontface = "bold" ), at = c(0,6.737538401,91.31779), labels = c("Negative", "Positive", "Third-quartile positive"), labels_gp= gpar(fontsize = 3, fontfamily = "Helvetica")))
 dev.off()
