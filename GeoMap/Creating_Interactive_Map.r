988f6b4984df87410c82e9cb020e8fba0e4fe67b
Sumy <50071183+sumyvbaby@users.noreply.github.com>
2022-02-08 09:22:12 +0000

    Update Creating_Interactive_Map.r

--- GeoMap/Creating_Interactive_Map.r
+++ GeoMap/Creating_Interactive_Map.r
@@ -27,7 +27,7 @@ saveWidget(widget = Map, file = "/home/sbaby/Desktop/Map.html")
 Data1 = read.table("/home/sbaby/Desktop/Latest_data/Map/MapPop.csv", header =TRUE, sep = ",")
 Data1 = as.data.frame(Data1) 
 pal1 <- colorQuantile(palette = "Dark2",domain = Data1$Population)
-map <- leaflet(countries) %>% addTiles()                             
+map <- leaflet(myfile)                             
 Map_Population <- map %>% setView(-3.475300, 55.89687, zoom = 8) %>% addPolygons(fillColor = ~pal(area),
                      fillOpacity = 0.8,
                      color = "#FF0000",
@@ -36,7 +36,7 @@ Map_Population <- map %>% setView(-3.475300, 55.89687, zoom = 8) %>% addPolygons
                                                         title = "Population", opacity = 1)%>% addCircles(lng = Data1$lon,lat = Data1$lat, label = Data1$Site,weight =8,color = ~pal1(Data1$Population),opacity = 1) 
 saveWidget(widget = Map_Population, file = "/home/sbaby/Desktop/Map_Population.html")                               
 Data1[Data1$Site %in% c("Allanfearn","Helensburgh","Carbarns","Hamilton","Philipshill","Seafield","East Calder","Linlithgow","Shieldhall","Dalmuir","Paisley","Daldowie","Nigg","Peterhead","Kirkwall","Lerwick","Hatton","Forfar","Stirling","Falkirk","Alloa","Meadowhead","Stevenston","Linlithgow","Dunfermline","Levenmouth","Kirkaldy","Troqueer","Lockerbie","Galashiels","Hawick","Stornoway"), ] -> Selected_Sites                              
-map <- leaflet(myfile) 
+map <- leaflet(myfile) %>% addTiles() 
 Map_Population1 <- map %>% setView(-3.475300, 55.89687, zoom = 10) %>% addPolygons(fillColor = ~pal(area),fillOpacity = 1,color = "#C0C0C0",weight = 1,label = myfile@data$name,labelOptions = labelOptions(noHide = F, textOnly = TRUE, style= list("font-family" = "Helvetica")))%>% addLegend(data= Data1,"bottomright",pal = pal1, values = Data1$Population,title = "Population", opacity = 1)%>% addCircles(lng = Data1$lon,lat = Data1$lat, label = Data1$Site,weight =8,color = ~pal1(Data1$Population),opacity = 1)%>% addLabelOnlyMarkers(lng = Selected_Sites$lon,lat = Selected_Sites$lat, label = Selected_Sites$Site,labelOptions = labelOptions(noHide = T, textOnly = TRUE,textsize = "12px", style = list("font-weight" = "bold","font-family" = "Helvetica")))                             
 saveWidget(widget = Map_Population1, file = "/home/sbaby/Desktop/Map_Population_2.html")
 Map_Population2<- map %>% addTiles() %>% setView(-3.475300, 55.89687, zoom = 10) %>% addPolygons(fillColor = ~pal(area),fillOpacity = 1,color = "#C0C0C0",weight = 1,label = myfile@data$name,labelOptions = labelOptions(noHide = F, textOnly = TRUE, style= list("font-family" = "Helvetica")))%>% addLegend(data= Data1,"bottomright",pal = pal1, values = Data1$Population,title = "Population", opacity = 1)%>% addCircles(lng = Data1$lon,lat = Data1$lat, label = Data1$Site,weight =8,color = ~pal1(Data1$Population),opacity = 1)%>% addLabelOnlyMarkers(lng = Selected_Sites$lon,lat = Selected_Sites$lat, label = Selected_Sites$Site,labelOptions = labelOptions(noHide = T, textOnly = TRUE,textsize = "12px", style = list("font-weight" = "bold","font-family" = "Helvetica")))                              
