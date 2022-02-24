f1e5dfd8adec486ca0c1c8f7ae5f08354d11b759
Sumy <50071183+sumyvbaby@users.noreply.github.com>
2022-02-24 11:12:00 +0000

    Update draw-heatmap.r

--- Prevalence-heatmap_All_Steps/draw-heatmap.r
+++ Prevalence-heatmap_All_Steps/draw-heatmap.r
@@ -24,6 +24,13 @@ quantile( Data5$Million.Gene.Copies.Per.Person.per.Day, na.rm=TRUE) # Calculate 
         0%        25%        50%        75%       100% 
     0.1600    19.9725    44.3200    89.0200 23570.7600 
 
+Data6<-(filter(Data, Data$N1.Reported.Value>=658 & Data$N1.Reported.Value<1316 )) # Filter for weak positive values
+quantile(Data6$Million.Gene.Copies.Per.Person.per.Day,na.rm =T) # Quantile for weak positives
+ 0%  25%  50%  75% 100%
+0.12 0.41 0.56 0.85 7.77  
+**[upper whisker end =1.5 [calculated as IQR *1.5 =.66 and the last value comes in the range .85 + .66 = 1.5 )
+
+
 # ************Binning normalized data on a weekly basis***************
 
 # Extracting the Year month and week information from dates
@@ -54,14 +61,13 @@ as.matrix(DF2) -> DF2
 Dates <- c("Dec-2020", "Jan-2021" ,"Feb-2021" ,"Mar-2021" ,"Apr-2021" ,"May-2021", "Jun-2021", "Jul-2021", "Aug-2021", "Sep-2021", "Oct-2021", "Nov-2021", "Dec-2021","Jan-2022", "Feb-2022")
 #Specify the annotation file for the bottom
 ha = HeatmapAnnotation(foo=anno_mark(at = c(1,6,10,14,19,24,29,34,39,44,49,54,59,64,69), labels = Dates,  labels_gp = gpar(fontsize=3, fontface = "bold"),which = "column", side = "bottom"))
-col_fun1 = colorRamp2(c(0,2.276259,89.0200), c("yellow", "orange","blue")) # 89.0200 is the third quantile value from the set of positivevalues for normalized data.Break points are set for the normalized values referring to the N1.Reported.Value range
 
+col_fun1 = colorRamp2(c(0,2.276259,89.0200), c("yellow", "orange","blue")) # 89.0200 is the third quantile value from the set of positivevalues for normalized data and 2.276259 is the 2nd minimum value for the positive range.[Break points are set for the normalized values referring to the N1.Reported.Value range] 
 jpeg(filename= "Covid_Heatmap_1.jpeg", width=1000, height = 700, units= "px",res= 300) # Save as jpeg with width 1000 pixels, height = 700 pixels, and res = 300 dpi
 Heatmap(DF2, col = col_fun1,row_names_gp = gpar(fontsize = 3, fontfamily = "Helvetica",fontface= "bold") , column_names_rot = 90,column_title = "Covid virus prevelance at different sites along the timeline",column_title_gp = gpar(fontsize = 5,fontface = "bold"),na_col = "white", cluster_rows = FALSE, cluster_columns = FALSE , bottom_annotation = ha, show_column_names = FALSE,border = TRUE,border_gp = gpar(col = "grey"), row_names_side = "left", heatmap_legend_param = list(title="Virus levels",title_gp = gpar(fontsize =3, fontfamily = "Helvetica", fontface = "bold" ), at = c(0,2.276259,89.0200), labels = c("Negative", "Positive", "Third-quartile positive"), labels_gp= gpar(fontsize = 3, fontfamily = "Helvetica")))
 dev.off()
 
-col_fun1 = colorRamp2(c(0,1.5,89.0200), c("yellow", "orange","blue")) # 89.0200 is the third quantile value from the set of positivevalues for normalized data.Break points are set for the normalized values referring to the N1.Reported.Value range
-
+col_fun1 = colorRamp2(c(0,1.5,89.0200), c("yellow", "orange","blue")) # 89.0200 is the third quantile value from the set of positivevalues for normalized data and 1.5 is the whisker end of the weak positive data as explained above. [Break points are set for the normalized values referring to the N1.Reported.Value range] 
 jpeg(filename= "Covid_Heatmap_2.jpeg", width=1000, height = 700, units= "px",res= 300) # Save as jpeg with width 1000 pixels, height = 700 pixels, and res = 300 dpi
 Heatmap(DF2, col = col_fun1,row_names_gp = gpar(fontsize = 3, fontfamily = "Helvetica",fontface= "bold") , column_names_rot = 90,column_title = "Covid virus prevelance at different sites along the timeline",column_title_gp = gpar(fontsize = 5,fontface = "bold"),na_col = "white", cluster_rows = FALSE, cluster_columns = FALSE , bottom_annotation = ha, show_column_names = FALSE,border = TRUE,border_gp = gpar(col = "grey"), row_names_side = "left", heatmap_legend_param = list(title="Virus levels",title_gp = gpar(fontsize =3, fontfamily = "Helvetica", fontface = "bold" ), at = c(0,1.5,89.0200), labels = c("Negative", "Positive", "Third-quartile positive"), labels_gp= gpar(fontsize = 3, fontfamily = "Helvetica")))
 dev.off() 
