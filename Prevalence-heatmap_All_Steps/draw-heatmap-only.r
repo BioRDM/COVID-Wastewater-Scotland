4b8c073bb086a5f0ab9444c4ca5bf50d5ae6842e
Tomasz Zielinski <tomasz.zielinski@ed.ac.uk>
2022-02-28 16:06:57 +0000

    figure for the paper

--- Prevalence-heatmap_All_Steps/draw-heatmap-only.r
+++ Prevalence-heatmap_All_Steps/draw-heatmap-only.r
@@ -33,11 +33,13 @@ fName = paste("prevalence_heatmap_",pos_start,".jpeg", sep = "")
 col_fun1 = colorRamp2(c(0,pos_start,pos_end), c("yellow", "orange","blue")) # 89.0200 is the third quantile value from the set of positivevalues for normalized data and 2.276259 is the 2nd minimum value for the positive range.[Break points are set for the normalized values referring to the N1.Reported.Value range] 
 jpeg(filename= fName, width=1000, height = 700, units= "px",res= 300) # Save as jpeg with width 1000 pixels, height = 700 pixels, and res = 300 dpi
 Heatmap(DF2, col = col_fun1, row_names_gp = gpar(fontsize = 3, fontfamily = "Helvetica",fontface= "bold") , 
-        column_names_rot = 90, column_title = "Covid virus prevelance at different sites along the timeline",
-        column_title_gp = gpar(fontsize = 5,fontface = "bold"), na_col = "white", cluster_rows = FALSE, cluster_columns = FALSE , 
+        column_names_rot = 90, 
+        #column_title = "Covid virus prevelance at different sites along the timeline",
+        #column_title_gp = gpar(fontsize = 5,fontface = "bold"), 
+        na_col = "white", cluster_rows = FALSE, cluster_columns = FALSE , 
         bottom_annotation = ha, show_column_names = FALSE, border = TRUE, border_gp = gpar(col = "grey"), 
         row_names_side = "left", 
-        heatmap_legend_param = list(title="Virus levels\n[Mgc/pp]", title_gp = gpar(fontsize =3, fontfamily = "Helvetica", fontface = "bold" ), at = c(0,2.276259,89.0200), 
+        heatmap_legend_param = list(title="Virus levels\n[Mgc/(person*day)]", title_gp = gpar(fontsize =3, fontfamily = "Helvetica", fontface = "bold" ), at = c(0,2.276259,89.0200), 
                                     labels = c("0", "positive", pos_end), labels_gp= gpar(fontsize = 3, fontfamily = "Helvetica")))
 
 dev.off()
