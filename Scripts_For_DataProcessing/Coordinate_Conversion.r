1a6bfa4e4d45674520d1a5cb9c42442923ae6303
Sumy <50071183+sumyvbaby@users.noreply.github.com>
2022-03-01 12:12:01 +0000

    Update Coordinate_Conversion.r

--- Scripts_For_DataProcessing/Coordinate_Conversion.r
+++ Scripts_For_DataProcessing/Coordinate_Conversion.r
@@ -1,8 +1,8 @@
 install.packages("sgo")
 library(sgo)
 
-Data = read.table("site-location-coordinates-LScorza.csv", header =TRUE, sep = ",")
-locs <- sgo_points(Data, coords=c("Long", "Lat"), epsg=27700)
+Data = read.table("Sites_coordinates_full.csv"", header =TRUE, sep = ",")
+locs <- sgo_points(Data, coords=c("Longitude_dd", "Latitude_dd"), epsg=27700)
 Data_sgo_4326 <- sgo_bng_lonlat(locs, to=4326)
 Data_4326 <- as.data.frame(Data_sgo_4326)
 colnames(Data_4326) = c("Longitude_dd", "Latitude_dd", "Health_Board","Site")
