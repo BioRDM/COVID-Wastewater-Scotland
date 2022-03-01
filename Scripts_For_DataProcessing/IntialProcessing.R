ec40d1420b6794a13634f867292e900ccadd76c2
Sumy <50071183+sumyvbaby@users.noreply.github.com>
2022-03-01 12:23:11 +0000

    Update IntialProcessing.R

--- Scripts_For_DataProcessing/IntialProcessing.R
+++ Scripts_For_DataProcessing/IntialProcessing.R
@@ -2,6 +2,6 @@ library(lubridate)
 Data = read.table("2022_02_14-RNA_Monitoring_Project_HB_Added.csv", header =TRUE, sep = ",")
 as.data.frame(Data) -> Data
 colnames(Data) <- c("Health_Board","Site","Date_collected","Date_analysed", "SW_sample_number","N1_description", "N1_Reported_value-gc_per_L","N1_Repl_1-gc_per_L","N1_Repl_2-gc_per_L","N1_Repl_3-gc_per_L","Calculated_mean","Standard_Deviation","Flow-L_per_day","Ammonia-mg_per_L","pH_value","Modelled_flow-L_per_day","Million_gene_copies_per_person_per_day","Analysis_lab")
-Data$Date_collected<- dmy(Data1$Date_collected) #Change the date format to YYYY-MM-DD
-Data$Date_analysed<- dmy(Data1$Date_analysed)   #Change the date format to YYYY-MM-DD
-write.csv(Data1,file="data_renamed.csv",quote = FALSE)
+Data$Date_collected<- dmy(Data$Date_collected) #Change the date format to YYYY-MM-DD
+Data$Date_analysed<- dmy(Data$Date_analysed)   #Change the date format to YYYY-MM-DD
+write.csv(Data,file="data_renamed.csv",quote = FALSE)
