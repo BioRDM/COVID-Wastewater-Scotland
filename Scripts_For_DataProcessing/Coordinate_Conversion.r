cf6bbeb48887e1cebf69c498fb7f6edbbf02693a
Sumy <50071183+sumyvbaby@users.noreply.github.com>
2022-03-01 13:12:15 +0000

    Included the codes in Intial Processing.R

--- Scripts_For_DataProcessing/Coordinate_Conversion.r
+++
@@ -1,9 +0,0 @@
-install.packages("sgo")
-library(sgo)
-
-Data = read.table("Sites_coordinates_full.csv"", header =TRUE, sep = ",")
-locs <- sgo_points(Data, coords=c("Longitude_dd", "Latitude_dd"), epsg=27700)
-Data_sgo_4326 <- sgo_bng_lonlat(locs, to=4326)
-Data_4326 <- as.data.frame(Data_sgo_4326)
-colnames(Data_4326) = c("Longitude_dd", "Latitude_dd", "Health_Board","Site")
-write.csv(Data_4326,file="sampling_sites.csv")
