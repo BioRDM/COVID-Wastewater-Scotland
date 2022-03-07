774160caa9c0bb12c87dbb6a019166d819d11784
Sumy <50071183+sumyvbaby@users.noreply.github.com>
2022-03-07 11:43:45 +0000

    Changed the annotation to match with the WW-YYYY data

--- Prevalence-heatmap_All_Steps/draw-heatmap.r
+++ Prevalence-heatmap_All_Steps/draw-heatmap.r
@@ -47,7 +47,7 @@ Data = read.table("Sample.csv", header =TRUE, sep = ",", row.names = 1,check.nam
 Data1 = as.matrix(Data)
 Data2 = Data1[,order(colnames(Data1))] # Sort columns 
 #Filtering by row name and columns
-Data2[row.names(Data2) %in% c("Allanfearn","Helensburgh","Carbarns","Hamilton","Philipshill","Seafield","East Calder","Linlithgow","Shieldhall","Dalmuir","Paisley","Daldowie","Nigg","Peterhead","Kirkwall","Lerwick","Hatton","Forfar","Stirling","Falkirk","Alloa","Meadowhead","Stevenston","Linlithgow","Dunfermline","Levenmouth","Kirkcaldy","Troqueer","Lockerbie","Galashiels","Hawick","Stornoway"),32:101] -> DF_new
+Data2[row.names(Data2) %in% c("Allanfearn","Helensburgh","Carbarns","Hamilton","Philipshill","Seafield","East Calder","Linlithgow","Shieldhall","Dalmuir","Paisley","Daldowie","Nigg","Peterhead","Kirkwall","Lerwick","Hatton","Forfar","Stirling","Falkirk","Alloa","Meadowhead","Stevenston","Linlithgow","Dunfermline","Levenmouth","Kirkcaldy","Troqueer","Lockerbie","Galashiels","Hawick","Stornoway"),29:90] -> DF_new
 # Convert the row.names to a column to facilitate the arrange function for custom sort 
 DF_new <- as.data.frame(DF_new)
 rownames_to_column(DF_new, var = "rowname") -> DF
@@ -58,7 +58,7 @@ column_to_rownames(DF1, var = "rowname") -> DF2
 as.matrix(DF2) -> DF2
 Dates <- c("Dec-2020", "Jan-2021" ,"Feb-2021" ,"Mar-2021" ,"Apr-2021" ,"May-2021", "Jun-2021", "Jul-2021", "Aug-2021", "Sep-2021", "Oct-2021", "Nov-2021", "Dec-2021","Jan-2022", "Feb-2022")
 #Specify the annotation file for the bottom
-ha = HeatmapAnnotation(foo=anno_mark(at = c(1,6,10,14,19,24,29,34,39,44,49,54,59,64,69), labels = Dates,  labels_gp = gpar(fontsize=3, fontface = "bold"),which = "column", side = "bottom"))
+ha = HeatmapAnnotation(foo=anno_mark(at = c(1,5,9,13,18,22,27,31,35,40,44,48,53,57,62), labels = Dates,  labels_gp = gpar(fontsize=3, fontface = "bold"),which = "column", side = "bottom"))
 
 col_fun1 = colorRamp2(c(0,2.276259,89.0200), c("yellow", "orange","blue")) # 89.0200 is the third quantile value from the set of positivevalues for normalized data and 2.276259 is the 2nd minimum value for the positive range.[Break points are set for the normalized values referring to the N1.Reported.Value range] 
 jpeg(filename= "Covid_Heatmap_1.jpeg", width=1000, height = 700, units= "px",res= 300) # Save as jpeg with width 1000 pixels, height = 700 pixels, and res = 300 dpi
